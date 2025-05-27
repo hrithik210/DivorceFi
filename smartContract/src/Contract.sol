766dfb6436d2ca5bfd947561d9f917322358b448
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-27 15:59:19 +0530

    basic contract init

--- smartContract/src/Contract.sol
+++ smartContract/src/Contract.sol
@@ -1,4 +1,26 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";  
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract RelationshipContract is ERC721, ReentrancyGuard, Ownable { 
+  uint tokenId = 1;
+
+  struct Relationship{
+    address partner1;
+    address partner2;
+    uint256 stakeAmount;
+    bool isActive;
+  }
+
+  mapping(uint => Relationship) public relationships;
+
+  constructor() ERC721("RelationshipToken", "REL") {
+
+  }
+
+
+}
