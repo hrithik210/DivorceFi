833138c6d07b00a02dcf4ee8e7c03a0bee5aff44
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-30 00:06:39 +0530

    init

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
   <div>
-    <h1 className="bg-red">Hello</h1>
+    <h1 className="bg-red-700">Hello</h1>
   </div> 
 );
 }
