49fedf79eb09f6d437316513b822945854d5328a
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-30 13:15:52 +0530

    wallet adapters

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { config } from '../config'
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -11,6 +15,9 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const queryClient = new QueryClient()
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,7 +34,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}> 
+            {children} 
+          </QueryClientProvider> 
+        </WagmiProvider>
       </body>
     </html>
   );
